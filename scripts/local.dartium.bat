51736a77b97ef02a45d9385f369632ca833737b6
Rory Claasen <rory@claasen.biz>
2016-10-16 15:15:42 +0100

    new html layout system

--- scripts/local.dartium.bat
+++ scripts/local.dartium.bat
@@ -1,4 +1,2 @@
 @echo off
-title Dart Shooter
-cd ../web/
-start "" "C:/Program Files/Dart/chromium/chrome.exe" --allow-file-access-from-files --app=file://%CD%/index.html
+start "" "C:/Program Files/Dart/chromium/chrome.exe" --allow-file-access-from-files --app=http://localhost:4000/web
