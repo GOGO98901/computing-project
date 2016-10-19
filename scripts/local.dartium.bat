2ce09228ceaa5c45c1a83c35fdbf5c93e8cc9eb1
Rory Claasen <rory@claasen.biz>
2016-10-19 17:49:24 +0100

    working on local fix for gettingpub packages to reapear

--- scripts/local.dartium.bat
+++ scripts/local.dartium.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:/Program Files/Dart/chromium/chrome.exe" --allow-file-access-from-files --app=http://localhost:4000/web
+start "" "C:/Program Files/Dart/chromium/chrome.exe" --use-spdy=off --allow-file-access-from-files --app=http://localhost:4000/web
