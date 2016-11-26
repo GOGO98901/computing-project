2cbd0a89a8a158eb59aa009fffbbfb5e9996c03e
Rory Claasen <rory@claasen.biz>
2016-11-26 20:08:36 +0000

    added pubspec to gh-pages

--- scripts/remote.deploy.sh
+++ scripts/remote.deploy.sh
@@ -20,6 +20,7 @@ if [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
 
 	cp README.md build/_site/static
 	cp LICENSE build/_site/static/license.txt
+	cp pubspec.yaml build/_site/static
 
 	cd build/_site/static
 	git init
