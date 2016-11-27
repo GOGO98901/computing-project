0044ec327b6c080e443e47d6c35d6fe9807708dc
Rory Claasen <rory@claasen.biz>
2016-11-27 18:32:02 +0000

    updated local.menu.bat script

--- scripts/local.menu.bat
+++ scripts/local.menu.bat
@@ -1,85 +1,94 @@
 @echo off
 set title=Development manager - Computing Project
-set doExit=0
+set /A doExit=0
 set bar=-------------------------------------------------------------------------------
 
 title %title%
 
 if NOT [%1] == [] (
-	set /a option=%1
-	set doExit=1
+	set /A option=%1
+	set /A doExit=1
 	goto command
 ) else (
 	cd ../
 )
 
-:license
-cls
-echo Copyright 2016 Rory Claasen
-echo Licensed under the Apache License, Version 2.0 (the "License");
-echo you may not use this file except in compliance with the License.
-echo You may obtain a copy of the License at
-echo http://www.apache.org/licenses/LICENSE-2.0
-echo Unless required by applicable law or agreed to in writing, software
-echo distributed under the License is distributed on an "AS IS" BASIS,
-echo WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-echo See the License for the specific language governing permissions and
-echo.
-pause
 
-:loop
+:menu
 title %title%
-if %doExit% == 1 (
-	exit
-)
-
 cls
 echo %bar%
 echo Main Menu - %title%
 echo %bar%
-echo   (0) exit
-echo   (1) pub get              - Collects the current dart dependencies
-echo   (2) jekyll build         - Builds the liquid html
-echo   (3) link dart            - Creates a symlink of the dart code to the output of (2).
-echo   (4) copy JavaScript      - Copy's the JavaScript to the output of (2).
-echo   (5) serve                - Starts a python server
-echo   (6) dartium              - Opens dartium browser
-echo   (7) clean _site          - Empties the _site directory
-echo   (9) license              - Shows the license
+echo General:
+echo   -  (0) Show license
+echo   -  (1) Get the pub dependencies
+echo   -  (2) Clean build directory
+echo   -  (3) Jekyll build
+echo After Jekyll:
+echo   -  (4) Link filesystem
+echo Non Dependent (but shoud be after Jekyll):
+echo   -  (5) Serve the build folder
+echo   -  (6) Open dartium
+
 echo %bar%
-set option=-1
-set /P option="To select an option enter the corresponding number: "
+set /A option=-1
+set /p option="To select an option enter the corresponding number: "
 
 :command
-title %title% - %option%
-if %option% == 0 (
-	exit
-) else (
-	if %option% == 1 (
-		start cmd /C pub get --packages-dir
-	)
-	if %option% == 2 (
-		start cmd /C jekyll build --config _config.yml,_config-dev.yml | cp dart _site/assets/script/
-	)
-	if %option% == 3 (
-		runas /user:Administrator /savecred "cmd /c mklink /J %CD%\dart\packages %CD%\packages"
-		runas /user:Administrator /savecred "cmd /c mklink /J %CD%\dart\util\packages %CD%\packages"
-		runas /user:Administrator /savecred "cmd /c mklink /J %CD%\_site\static\assets\script\dart %CD%\dart"
-	)
-	if %option% == 4 (
-		cp -r static/assets/script/javascript/ _site/static/assets/script/
-	)
-	if %option% == 5 (
-		start cmd /k "cd _site/static && python -m SimpleHTTPServer 3694"
-	)
-	if %option% == 6 (
-		start "" "C:/Program Files/Dart/chromium/chrome.exe" --use-spdy=off --allow-file-access-from-files --app=http://127.0.0.1:3694/
-	)
-	if %option% == 7 (
-		rd /s /q "_site/"
-	)
-	if %option% == 9 (
-		goto license
-	)
+cls
+title %title% - [option = %option%]
+if %option%==0 (
+	echo Copyright 2016 Rory Claasen
+	echo Licensed under the Apache License, Version 2.0 (the License);
+	echo you may not use this file except in compliance with the License.
+	echo You may obtain a copy of the License at
+	echo http://www.apache.org/licenses/LICENSE-2.0
+	echo Unless required by applicable law or agreed to in writing, software
+	echo distributed under the License is distributed on an AS IS BASIS,
+	echo WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	echo See the License for the specific language governing permissions and
+	pause
 )
-goto loop
+if %option%==1 (
+	:: Collects pub dependencies and saves them
+	start cmd /C pub get --packages-dir
+)
+if %option%==2 (
+	:: Deletes the build directory
+	rd /s /q "_site/"
+)
+if %option%==3 (
+	:: Builds the liquid html
+	start cmd /C jekyll build --config _config.yml,_config-dev.yml
+)
+if %option%==4 (
+	:: Copy's pubspec
+	cp _site\pubspec.yaml _site\static
+
+	:: linsk dart to script directory
+	mkdir  %CD%\_site\static\assets\script\
+	runas /user:Administrator /savecred "cmd /c mklink /J %CD%\_site\static\assets\script\dart %CD%\dart"
+
+	:: links packages to dart dir
+	runas /user:Administrator /savecred "cmd /c mklink /J %CD%\dart\packages %CD%\packages"
+	runas /user:Administrator /savecred "cmd /c mklink /J %CD%\dart\util\packages %CD%\packages"
+
+	:: links javascript to script directory
+	runas /user:Administrator /savecred "cmd /c mklink /J %CD%\_site\static\assets\script\javascript %CD%\static\assets\script\javascript"
+
+	:: link images to assets directory
+	runas /user:Administrator /savecred "cmd /c mklink /J %CD%\_site\static\assets\images %CD%\static\assets\images"
+)
+if %option%==5 (
+	:: Starts python webserver
+	start cmd /k "cd _site/static && python -m SimpleHTTPServer 3694"
+)
+if %option%==6 (
+	:: Starts dartium
+	start "" "C:/Program Files/Dart/chromium/chrome.exe" --use-spdy=off --allow-file-access-from-files --app=http://127.0.0.1:3694/
+)
+if %doExit%==1 (
+	::exit
+)
+goto menu
