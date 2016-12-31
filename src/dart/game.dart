67594ace66e8441690fe6af4546cc786bfbc0ec2
Rory Claasen <rory@claasen.biz>
2016-12-31 20:44:46 +0000

    removed jekyll test

--- src/dart/game.dart
+++ src/dart/game.dart
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-part of Computer_Science_Prodject;
+part of Computer_Science_Project;
 
 class GameHost {
 	final CanvasElement _canvas;
