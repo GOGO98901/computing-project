0a7533b789feaafd73b72ca38d76b4471f3295b8
Rory Claasen <rory@claasen.biz>
2017-01-26 18:11:28 +0000

    added todo

--- src/dart/util/toolbox2.dart
+++ src/dart/util/toolbox2.dart
@@ -54,4 +54,5 @@ class JsonList extends JsonObject implements List {
 class Regex {
 	RegExp get vars => new RegExp(r'\$([^0]\d+|[1-9])');
 	RegExp get token => new RegExp(r'(\b\w{3,4}\B|admin)\d+');
+	// TODO make sure that it wont except 'test01' but wil 'test1'
 }
