6326b2077c3e449bf16c582adaeeb2a12a012e45
Rory Claasen <rory@claasen.biz>
2017-03-02 12:12:48 +0000

    more work on the new problems

--- src/dart/problem.dart
+++ src/dart/problem.dart
@@ -19,25 +19,43 @@ class ProblemManager {
     JsonFile _dataFile;
 
     HashMap<int, List<ProblemItem>> _problems;
+    List<ProblemTypeItem> _typeProblems;
+
     ProblemManager() {
         _problems = new HashMap<int, List<ProblemItem>>();
+        _typeProblems = new List<ProblemTypeItem>();
         _dataFile = ResourceManager.sample;
 
         _dataFile.onLoad.then((e) {
-            JsonList list = new JsonList.fromString(e['problems'].toString());
-            for (JsonObject problem in list) {
-                int level = problem.level;
-                String question = problem.data.question;
-                int answer = problem.data.answer;
-                List<String> options = problem.data.options;
+            loadProblems(new JsonList.fromString(e['problems'].toString()));
+            loadTypeProblems(new JsonList.fromString(e['typeProblems'].toString()));
 
-                if (!_problems.containsKey(level)) _problems[level] = new List<ProblemItem>();
-                List<ProblemItem> list = _problems[level];
-                list.add(new ProblemItem(question, options, answer));
-                _problems[level] = list;
-            }
             // log.info(_problems);
         });
+    }
+
+    void loadProblems(JsonList list) {
+        for (JsonObject problem in list) {
+            int level = problem.level;
+            String question = problem.data.question;
+            int answer = problem.data.answer;
+            List<String> options = problem.data.options;
+
+            if (!_problems.containsKey(level)) _problems[level] = new List<ProblemItem>();
+            List<ProblemItem> list = _problems[level];
+            list.add(new ProblemItem(question, options, answer));
+            _problems[level] = list;
+        }
+    }
+
+    void loadTypeProblems(JsonList list) {
+        for (JsonObject problem in list) {
+            String question = problem.question;
+            List<int> answers = problem.answers;
+            List<String> options = problem.data.options;
+
+            _typeProblems.add(new ProblemTypeItem(question, options, answers));
+        }
     }
 }
 
@@ -59,7 +77,28 @@ class ProblemItem {
         return {
             "question": _question,
             "options": _options,
-            "anseer": _answer
+            "answer": _answer
+        }.toString();
+    }
+}
+
+class ProblemTypeItem {
+    String _question;
+    List<String> _options;
+    List<int> _answers;
+
+    ProblemTypeItem(this._question, this._options, this._answers);
+
+    String get question => _question;
+
+    List<String> get questions => _options;
+    List<int> get answers => _answers;
+
+    String toString() {
+        return {
+            "question": _question,
+            "options": _options,
+            "answers": _answers
         }.toString();
     }
 }
